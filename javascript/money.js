818c325d7486223fb66c270f6bc4a8fed3a8b6e5
gazifahad <gazifahad3@gmail.com>
2022-02-17 21:32:41 +0600

    finished

--- javascript/money.js
+++ javascript/money.js
@@ -13,6 +13,7 @@ function expenseCalculate(){
     const rentExpense=getInput('rent-cost');
     const clothExpense=getInput('cloth-cost');
     const totalExpense=parseFloat(foodExpense) + parseFloat(rentExpense) +parseFloat(clothExpense);
+    // error message for negative expense value
     if(foodExpense<0 || rentExpense<0 || clothExpense<0){
       document.getElementById('expense-error').style.display='block';
      
@@ -36,15 +37,23 @@ document.getElementById('calculate-button').addEventListener('click',function(){
     // get the income value and assign the remaining balance in the allocated field 
     const income=getInput('total-income');
     const remainingBalance= parseFloat(income)-expensed;
+    // error alert for negative income value 
+   if(income<0){
+     alert("you cannot earn negative! enter a positive value!")
+
+
+   }
+   else{
     if(remainingBalance>0){
-    document.getElementById('balance-calculate').innerText=remainingBalance;
-    document.getElementById('expense-greater-error').style.display='none';
-    }
-    else if (remainingBalance<0){
-      {
-        document.getElementById('expense-greater-error').style.display='block';
+      document.getElementById('balance-calculate').innerText=remainingBalance;
+      document.getElementById('expense-greater-error').style.display='none';
       }
-    }
+      else if (remainingBalance<0){
+        {
+          document.getElementById('expense-greater-error').style.display='block';
+        }
+      }
+   }
 
 })
 // event listener for save button 
@@ -52,14 +61,23 @@ document.getElementById('calculate-save-button').addEventListener('click',functi
   // get the saving amount of income 
   const income=getInput('total-income');
   const saving=getInput('save-percentage');
+  const expensed= expenseCalculate();
+  const remainingBalance= income-expensed;
   const savingAmount= (income*saving)/100;
+  if(savingAmount<remainingBalance){
   // assign the saving amount in allocated field
   document.getElementById("saving-amount-calculate").innerText=savingAmount;
-  const expensed= expenseCalculate();
+  
   // calculate the balance after saving and assign it in allocated field 
-  const remainingBalance= income-expensed;
+  
   const lastBalance =  remainingBalance-savingAmount;
   document.getElementById('remaining-amount-calculate').innerText=lastBalance;
-  
+  document.getElementById('saving-alert').style.display='none';
+}
+// push error message for saving amount more than balance 
+else{
+  document.getElementById('saving-alert').style.display='block';
+
+}
 
 })
